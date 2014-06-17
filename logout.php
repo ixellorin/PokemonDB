0f18050353c74135549319d9b4b9bb12c1a60bff
Johnnie <johnnie-l@live.ca>
2014-06-17 16:12:08 -0700

    Updated logout redirection
    - Login bug, not universal (try logging in and searching)

--- logout.php
+++ logout.php
@@ -1,6 +1,18 @@
+<html>
+<head>
+<title>PokemonDB - Logging Out</title>
+<link rel="stylesheet" type="text/css" href="template.css"/>
+</head>
+<body>
+<div id="login">
+</div>
+<center>
 <?php
 session_start();
 session_destroy();
-header("Location:/pokemondb ");
+echo "You have logged out. Returning you to homepage.";
+echo "<script>setTimeout(\"location.href = '/pokemondb';\",1500);</script>";
 die();
 ?>
+</center>
+</html>
