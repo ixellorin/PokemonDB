fd8930c6f211530e7131afc1dc5b7a2cd12126b2
iellorin <iellorin@hotmail.com>
2014-06-17 13:16:41 -0700

    Removed Unnecessary Table
    Merged the WeakAgainst/StrongAgainst Tables into one Matchups table

--- PokeDB.sql
+++ PokeDB.sql
@@ -9,7 +9,7 @@ drop table if exists PokemonMoves;
 drop table if exists StrongAgainst;
 drop table if exists Trainer;
 drop table if exists Type;
-drop table if exists WeakAgainst;
+drop table if exists Matchups;
 drop table if exists Species;
 
 CREATE TABLE Area
@@ -66,14 +66,7 @@ PRIMARY KEY(pid, mName),
 FOREIGN KEY (pid) REFERENCES Pokemon(Pokemon_ID),
 FOREIGN KEY (mName) REFERENCES Moves(mName));
 
-CREATE TABLE WeakAgainst(
-attack_type_name VARCHAR(20),
-defend_type_name VARCHAR(20),
-PRIMARY KEY (attack_type_name, defend_type_name),
-FOREIGN KEY (attack_type_name) REFERENCES Type(name),
-FOREIGN KEY (defend_type_name) REFERENCES Type(name));
-	   
-CREATE TABLE StrongAgainst(
+CREATE TABLE Matchups(
 attack_type_name VARCHAR(20),
 defend_type_name VARCHAR(20),
 PRIMARY KEY (attack_type_name, defend_type_name),
@@ -241,5 +234,241 @@ insert into Type values
 
 insert into DBManager values
 (00000003, 'oak');
+
+
+-- WEAK against
+
+insert into Matchups values
+('Fighting (S)', 'Normal (W)');
+
+insert into Matchups values
+('Normal (W)', 'Rock (S)');
+
+insert into Matchups values
+('Normal (W)', 'Ghost (S)');
+
+insert into Matchups values
+('Fighting (W)', 'Flying (S)');
+
+insert into Matchups values
+('Fighting (W)', 'Poison (S)');
+
+insert into Matchups values
+('Fighting (W)', 'Bug (S)');
+
+insert into Matchups values
+('Fighting (W)', 'Ghost (S)');
+
+insert into Matchups values
+('Fighting (W)', 'Psychic (S)');
+
+insert into Matchups values
+('Flying (S)','Fighting (W)');
+
+insert into Matchups values
+('Flying (W)', 'Rock (S)');
+
+insert into Matchups values
+('Flying (W)', 'Electric (S)');
+
+insert into Matchups values
+('Poison (W)', 'Poison (S)');
+
+insert into Matchups values
+('Poison (W)', 'Ground (S)');
+
+insert into Matchups values
+('Poison (W)', 'Rock (S)');
+
+insert into Matchups values
+('Poison (W)', 'Ghost (S)');
+
+insert into Matchups values
+('Ground (W)', 'Flying (S)');
+
+insert into Matchups values
+('Ground (W)', 'Bug (S)');
+
+insert into Matchups values
+('Ground (W)', 'Grass (S)');
+
+insert into Matchups values
+('Rock (W)', 'Fighting (S)');
+
+insert into Matchups values
+('Rock (W)', 'Ground (S)');
+
+insert into Matchups values
+('Bug (W)', 'Fighting (S)');
+
+insert into Matchups values
+('Bug (W)', 'Flying (S)');
+
+insert into Matchups values
+('Bug (W)', 'Ghost (S)');
+
+insert into Matchups values
+('Bug (W)', 'Fire (S)');
+
+insert into Matchups values
+('Ghost (W)', 'Normal (S)');
+
+insert into Matchups values
+('Ghost (W)', 'Psychic (S)');
+
+insert into Matchups values
+('Fire (W)', 'Rock (S)');
+
+insert into Matchups values
+('Fire (W)', 'Fire (S)');
+
+insert into Matchups values
+('Fire (W)', 'Water (S)');
+
+insert into Matchups values
+('Fire (W)', 'Dragon (S)');
+
+insert into Matchups values
+('Water (W)', 'Water (S)');
+
+insert into Matchups values
+('Water (W)', 'Grass (S)');
+
+insert into Matchups values
+('Water (W)', 'Dragon (S)');
+
+insert into Matchups values
+('Grass (W)', 'Flying (S)');
+
+insert into Matchups values
+('Grass (W)', 'Bug (S)');
+
+insert into Matchups values
+('Grass (W)', 'Fire (S)');
+
+insert into Matchups values
+('Grass (W)', 'Grass (S)');
+
+insert into Matchups values
+('Grass (W)', 'Dragon (S)');
+
+insert into Matchups values
+('Psychic (W)', 'Psychic (S)');
+
+insert into Matchups values
+('Ice (W)', 'Water (S)');
+
+insert into Matchups values
+('Ice (W)', 'Ice (S)');
+
+insert into Matchups values
+('Psychic (S)','Fighting (W)');
+
+insert into Matchups values
+('Rock (S)','Flying (W)');
+
+insert into Matchups values
+('Electric (S)','Flying (W)');
+
+insert into Matchups values
+('Ice (S)', 'Flying (W)');
+
+insert into Matchups values
+('Ground (S)', 'Poison (W)');
+
+insert into Matchups values
+('Bug (S)', 'Poison (W)');
+
+insert into Matchups values
+('Psychic (S)', 'Poison (W)');
+
+insert into Matchups values
+('Water (S)', 'Ground (W)');
+
+insert into Matchups values
+('Grass (S)', 'Ground (W)');
+
+insert into Matchups values
+('Ice (S)', 'Ground (W)');
+
+insert into Matchups values
+('Fighting (S)', 'Rock (W)');
+
+insert into Matchups values
+('Ground (S)', 'Rock (W)');
+
+insert into Matchups values
+('Water (S)', 'Rock (W)');
+
+insert into Matchups values
+('Grass (S)', 'Rock (W)');
+
+insert into Matchups values
+('Flying (S)', 'Bug (W)');
+
+insert into Matchups values
+('Poison (S)', 'Bug (W)');
+
+insert into Matchups values
+('Rock (S)', 'Bug (W)');
+
+insert into Matchups values
+('Fire (S)', 'Bug (W)');
+
+insert into Matchups values
+('Ghost (S)', 'Ghost (W)');
+
+insert into Matchups values
+('Ground (S)', 'Fire (W)');
+
+insert into Matchups values
+('Rock (S)', 'Fire (W)');
+
+insert into Matchups values
+('Water (S)', 'Fire (W)');
+
+insert into Matchups values
+('Grass (S)', 'Water (W)');
+
+insert into Matchups values
+('Electric (S)', 'Water (W)');
+
+insert into Matchups values
+('Flying (S)', 'Grass (W)');
+
+insert into Matchups values
+('Poison (S)', 'Grass (W)');
+
+insert into Matchups values
+('Bug (S)', 'Grass (W)');
+
+insert into Matchups values
+('Fire (S)', 'Grass (W)');
+
+insert into Matchups values
+('Ice (S)', 'Grass (W)');
+
+insert into Matchups values
+('Ground (S)','Electric (W)');
+
+insert into Matchups values
+('Bug (S)', 'Psychic (W)');
+
+insert into Matchups values
+('Fighting (S)', 'Ice (W)');
+
+insert into Matchups values
+('Rock (S)', 'Ice (W)');
+
+insert into Matchups values
+('Fire (S)', 'Ice (W)');
+
+insert into Matchups values
+('Ice (S)', 'Dragon (W)');
+
+insert into Matchups values
+('Dragon (S)', 'Dragon (W)');
+
+-- ----------------------------------------------------------------------------
 
 set Foreign_key_checks=1;
