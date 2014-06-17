8134e69923a36aa8c81de9d026280bdbff861554
iellorin <iellorin@hotmail.com>
2014-06-17 16:55:16 -0700

    Added sorting for matchups
    only Strong Attacks doesn't work

--- search.php
+++ search.php
@@ -25,9 +25,23 @@
 	<option value="Ptype"> Type</option>
 	<option value="PSpecies"> Species</option>
 </select>
+
+
+<p> Sort matchups by:
+<select name = "matchup_category">
+	<option value="attack_type_name"> Attacking Type</option>
+	<option value="defend_type_name"> Defending Type</option>
+	<option value="Attack_Strong"> Strong Attacks</option>
+	<option value="Defend_Strong"> Strong Defends</option>
+</select>
+
+
 <input type="hidden" name="searching" value="yes" />
 <input type="submit" name="search" value="Search" />
 </form>
+
+
+
 
 <?php
 session_start();
@@ -35,6 +49,7 @@ session_start();
  $searching = $_POST['searching'];
  $find = $_POST['find'];
  $category = $_POST['category'];
+ $matchup_category = $_POST['matchup_category'];
  
  echo "<h2>Search Results</h2><p>"; 
   
@@ -54,8 +69,16 @@ session_start();
  
  //Now we search for our search term, in the field the user specified 
  
-  if(preg_match('/^MATCHUP(S)?/',$find)){
-	$query = "SELECT * FROM Matchups";
+  if(preg_match('/^MATCHUP(S)?/', $find)){
+  if (preg_match('/^Attack_Strong/', $matchup_category)){
+	$query = "SELECT * FROM Matchups Where attack_type_name like '%(S)%' and defend_type_name like '%(W)%' Order by attack_type_name";
+	}
+  if (preg_match('/^Defend_Strong/', $matchup_category)){
+	$query = "SELECT * FROM Matchups Where attack_type_name like '%(W)%' and defend_type_name like '%(S)%' Order by defend_type_name";
+	}
+	else{
+	$query = "SELECT * FROM Matchups ORDER BY $matchup_category";
+	}	
 	$result = mysqli_query($con, $query);
 
 echo "<table border='1'>
