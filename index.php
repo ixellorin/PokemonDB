c6d43f113cfab93c27bdb21244b9713aa26a6f49
Johnnie <johnnie-l@live.ca>
2014-06-17 17:39:43 -0700

    Fixed login
    - Still need to do DBManager search + add / remove trainer stuff

--- index.php
+++ index.php
@@ -4,21 +4,40 @@
 <link rel="stylesheet" type="text/css" href="template.css"/>
 </head>
 <body>
+<?php
+    session_start();
+    if(!isset($_SESSION['trainer_ID']))
+    {
+?>
+
 <div id="login">
-  <form method="POST" action="<?php htmlspecialchars("PHP_SELF", ENT_QUOTES,"UTF-8"); ?>">
+  <form method="POST" action="index.php">
     User: <input type="text" name="username" size="14" maxlength="30" placeholder="Trainer ID" />
     Password: <input type="password" name="password" size="14" maxlength="30" />
     <input type="submit" value="Log In" name="loginButton" />
   </form>
 </div>
 
+<?php 
+    }
+    else
+    {
+?>
+
+<div id="login">
+	You are logged in.
+	<button><a href="logout.php" style="text-decoration: none">Log Out</a></button>
+</div>
+
+<?php
+	}
+?>
+
 <a href="index.php"><img src="PokemonLogo.png"></a>
 
 <?php
 echo "Welcome to the PokemonDB";
 echo "<br>";
-	
-session_start();
  
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  	$username=htmlspecialchars($_POST['username'],ENT_QUOTES,"UTF-8"); 
@@ -68,44 +87,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 	<option value="Ptype"> Type</option>
 	<option value="PSpecies"> Species</option>
 </select>
-
-<p> Sort matchups by:
-<select name = "matchup_category">
-	<option value="Attack_Type"> Attacking Type</option>
-	<option value="Defend_Type"> Defending Type</option>
-	<option value="Attack_Strong"> Strong Attacks</option>
-	<option value="Defend_Strong"> Strong Defends</option>
-</select>
-
-<p> Show me the types 
-<select name = "type">
-	<option value="Normal">Normal</option>
-	<option value="Fighting">Fighting</option>
-	<option value="Flying">Flying</option>
-	<option value="Poison">Poison</option>
-	<option value="Ground">Ground</option>
-	<option value="Rock">Rock</option>
-	<option value="Bug">Bug</option>
-	<option value="Ghost">Ghost</option>
-	<option value="Fire">Fire</option>
-	<option value="Water">Water</option>
-	<option value="Grass">Grass</option>
-	<option value="Electric">Electric</option>
-	<option value="Psychic">Psychic</option>
-	<option value="Ice">Ice</option>
-	<option value="Dragon">Dragon</option>	
-</select>	
-	is 
-<select name = "weak_or_strong">
-	<option value="(S)">strong</option>
-	<option value="(W)">weak</option>
-</select>
-	against when 
-<select name = "attack_or_defend">
-	<option value="attacking">attacking</option>
-	<option value="defending">defending</option>
-</select>
-
 <input type="hidden" name="searching" value="yes" />
 <input type="submit" name="search" value="Search" />
 </form>
